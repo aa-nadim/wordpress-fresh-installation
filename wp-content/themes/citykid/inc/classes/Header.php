e4a5dcaf3038e62e274b4406b4300695a2c6a45b
Nadim <nadim@themeperch.net>
2024-06-30 11:58:32 +0600

    not error

--- wp-content/themes/citykid/inc/classes/Header.php
+++ wp-content/themes/citykid/inc/classes/Header.php
@@ -155,17 +155,7 @@ final class Header{
 				}		
 			)
 		);
-		// banner_bg_color
-		$wp_customize->add_setting(
-			'nav_button_style',
-            array(
-				'capability'        => 'edit_theme_options',
-				'default'           => 'btn-primary',				
-				'sanitize_callback' => static function( $value ) {
-					return esc_attr($value);
-				}
-			)			
-		);
+		
 
 		
 
@@ -216,99 +206,7 @@ final class Header{
 				'label'   => esc_html__( 'Enable sticky navbar', 'citykid' ),
 			)
 		);
-
-
-        
-
-        // Banner image
-		$wp_customize->add_setting(
-			'banner_image',
-			array(
-				'sanitize_callback' => array( __NAMESPACE__.'\\Customize', 'sanitize_attachment' )
-			)
-			
-		);
-
-		$wp_customize->add_control( new \WP_Customize_Media_Control(
-			$wp_customize, 'banner_image', 
-			array( // setting id
-				'label'         => esc_attr__( 'Banner image', 'citykid' ),
-				'description'   => esc_attr__( 'Every page have own banner settings.', 'citykid' ),
-				'frame_title'   => esc_attr__( 'Select banner image', 'citykid' ),
-				'mime_type'     => 'image',
-                'flex_width'    => true, // Allow any width, making the specified value recommended. False by default.
-                'flex_height'   => false, // Require the resulting image to be exactly as tall as the height attribute (default).
-                'width'         => 1920,
-                'height'        => 1080,
-				'section'       => 'header_image',
-			)
-		) );
-
-        // banner_image_position
-		$wp_customize->add_setting(
-			'banner_image_position',
-            array(
-				'capability'        => 'edit_theme_options',
-				'default'           => 'center center',				
-				'sanitize_callback' => static function( $value ) {
-					return esc_attr($value);
-				},
-			)
-            
-			
-		);
-		$wp_customize->add_control( 
-            'banner_image_position', 
-            array(
-                'type'          => 'select',
-                'section'       => 'header_image',
-                'label'   	=> esc_html__( 'Banner image position', 'citykid' ),
-				'choices' 	=> array(
-					'left top' 		=> esc_html__( 'Top Left', 'citykid' ),
-					'center top'   => esc_html__( 'Top', 'citykid' ),
-					'right top'   => esc_html__( 'Top Right', 'citykid' ),
-					'left center'   => esc_html__( 'Left', 'citykid' ),
-					'center center'   => esc_html__( 'Center', 'citykid' ),
-					'right center'   => esc_html__( 'Right', 'citykid' ),
-					'left bottom'   => esc_html__( 'Bottom Left', 'citykid' ),
-					'center bottom'   => esc_html__( 'Bottom', 'citykid' ),
-					'right bottom'   => esc_html__( 'Bottom Right', 'citykid' ),
-				),
-                'active_callback' => static function() {
-					return get_theme_mod('banner_image', false)? true : false;
-				}
-            ) 
-        );
-
-        // banner_image_opacity
-		$wp_customize->add_setting(
-			'banner_image_opacity',
-			array(
-				'capability'        => 'edit_theme_options',
-				'default'           => 25,				
-				'sanitize_callback' => static function( $value ) {
-					return intval($value);
-				},
-				
-			)
-		);
-
-		$wp_customize->add_control( 
-            'banner_image_opacity', 
-            array(
-                'type'          => 'range',
-                'section'       => 'header_image',
-                'label'         => esc_attr__( 'Banner image opacity', 'citykid' ),
-                'input_attrs'   => array(
-                    'min'   => 0,
-                    'max'   => 100,
-                    'step'  => 1,
-                ),
-				'active_callback' => static function() {
-					return get_theme_mod('banner_image', false)? true : false;
-				}
-          ) 
-        );      
+    
 		
 		
         return $wp_customize;
